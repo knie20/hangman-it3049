875ce7a3dc28d4717ae911c9e7037d6e84669ddd
kaidong nie <niekg@mail.uc.edu>
2018-02-21 16:00:14 -0500

    finished for now

--- game.js
+++ game.js
@@ -5,9 +5,11 @@ const textGuessingBox = document.getElementById("textGuessingBox")
 const guessForm = document.getElementById("guessForm")
 const restartBtn = document.getElementById("restartBtn")
 const difficultyLvl = document.getElementById("difficultyLvl")
-const guessedLetters = document.getElementById("guessedLetters")
+const guessedLettersDiv = document.getElementById("guessedLetters")
 
 difficultyLvl.innerHTML = sessionStorage.getItem("difficulty")
+
+let ctx = hangman.getContext('2d')
 
 
 async function getRandomWord(difficulty){
@@ -39,9 +41,11 @@ class HangmanGame {
         console.log(this.word)
         this.wordArray = this.word.split('')
         this.wordLength = this.wordArray.length
-        this.numberOfGuesses = 0
-        this.hangmanLife = 6
-        this.lettersGuessed = 0
+        this.hangmanLife = 5
+        this.lettersGuessedRight = 0
+        this.lettersGuessed = ''
+
+        this.drawStand()
 
         this.wordArray.forEach(char => {
             textGuessBox.innerHTML += '_ '
@@ -51,14 +55,20 @@ class HangmanGame {
     }
 
     guess(char){
-        this.numberOfGuesses++
         var guessCorrect = false
-        guessedLetters.innerHTML += char + ' '
+
+        if(!this.lettersGuessed.includes(char)){
+            this.lettersGuessed += char
+            guessedLettersDiv.innerHTML = "Your Guesses:" + this.lettersGuessed
+        }else{
+            alert("this letter has already been guessed")
+            return
+        }
 
         if(this.hangmanLife > 0){
             for (let i = 0; i < this.wordLength; i++) {
                 if(char == this.wordArray[i]){
-                    this.lettersGuessed++
+                    this.lettersGuessedRight++
                     guessCorrect = true
                     var displaytxt = textGuessBox.innerHTML
                     textGuessBox.innerHTML = displaytxt.substring(0, i * 2) + char + " " + displaytxt.substring((i + 1) * 2, displaytxt.length)
@@ -66,13 +76,15 @@ class HangmanGame {
             }
 
             if(guessCorrect === false){
+                this.drawMan(this.hangmanLife)
                 this.hangmanLife--
             }
 
-            if(this.lettersGuessed === this.wordLength){
+            if(this.lettersGuessedRight === this.wordLength){
                 this.gameWon()
             }
         }else{
+            this.drawMan(this.hangmanLife)
             this.gameLost()
         }
     }
@@ -89,6 +101,56 @@ class HangmanGame {
         restartBtn.innerHTML = "You have won! Restart?"
         restartBtn.style.display = 'block'
 
+    }
+
+    drawStand(){
+        ctx.beginPath()
+        ctx.moveTo(15, 175)
+        ctx.lineTo(15, 25)
+        ctx.lineTo(100, 25)
+        ctx.lineTo(100, 40)
+        ctx.stroke()
+    }
+
+    drawMan(hangmanLife){
+        switch (hangmanLife) {
+            case 5:
+                ctx.arc(100, 60, 20, 0, Math.PI * 2, false)
+                ctx.stroke()
+                break;
+            case 4:
+                ctx.beginPath()
+                ctx.moveTo(100, 80)
+                ctx.lineTo(100, 120)
+                ctx.stroke()
+                break;
+            case 3:
+                ctx.beginPath()
+                ctx.moveTo(100, 80)
+                ctx.lineTo(80, 100)
+                ctx.stroke()
+                break;
+            case 2:
+                ctx.beginPath()
+                ctx.moveTo(100, 80)
+                ctx.lineTo(120, 100)
+                ctx.stroke()
+                break;
+            case 1:
+                ctx.beginPath()
+                ctx.moveTo(100, 120)
+                ctx.lineTo(80, 140)
+                ctx.stroke()
+                break;
+            case 0:
+                ctx.beginPath()
+                ctx.moveTo(100, 120)
+                ctx.lineTo(120, 140)
+                ctx.stroke()
+                break;
+            default:
+                break;
+        }
     }
 }
 
